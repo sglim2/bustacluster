ae09f2758ecce56c1d07c0e02f9e7b85fc5639a6
Ian Merrick <MerrickI@Cardiff.ac.uk>
2025-04-09 19:57:21 +0100

    working on k8s-basic; moving the kubeadm commands to the VM, away from the host.

--- virt/k8s-basic/deploy.sh
+++ virt/k8s-basic/deploy.sh
@@ -10,19 +10,18 @@ CLUSTERNAME=${CLUSTERNAME:=k8s}
 withCNI=${withCNI:=cilium}
 withPROMETHEUS=${withPROMETHEUS:=0}
 withMETRICS=${withMETRICS:=0}
-kubeVER=${kubeVER:=1.28.7}
+kubeVER=${kubeVER:=1.32.0}
 maxpods=${maxpods:=110}
 
-
 nVMS=${nVMS:=3}
-IMAGENAME=${IMAGENAME:='rocky8-bustacluster'}
+IMAGENAME=${IMAGENAME:='rocky9'}
 
 source ../basic/deploy.sh
 
 #================================================================================================
 # prepare node with ansible
-echo ansible-playbook -i $( echo "${IPS[*]}" ) ../k8s-basic/playbook.yaml --private-key ../repo-builder/imgs/bustacluster-key --ssh-extra-args=\"-o StrictHostKeyChecking=no\"
-ansible-playbook -i "$( IFS=$','; echo "${IPS[*]}", )"  ../k8s-basic/playbook.yaml --private-key ../repo-builder/imgs/bustacluster-key --ssh-extra-args="-o StrictHostKeyChecking=no"
+echo ansible-playbook -i $( echo "${IPS[*]}" ) ../k8s-basic/playbook-k8s.yaml
+ansible-playbook -i "$( IFS=$','; echo "${IPS[*]}", )"  ../k8s-basic/playbook-k8s.yaml 
 #================================================================================================
 
 
@@ -40,37 +39,31 @@ echo "withPROMETHEUS  = $withPROMETHEUS"
 echo "kubeVER         = $kubeVER"
 echo "=================================="
 
-# remove any possible offending host keys from known_hosts
-for ip in ${IPS[*]}; do
-  ssh-keygen -R $ip
-  # (re)populate known_hosts
-  ssh -o StrictHostKeyChecking=no -i ../repo-builder/imgs/bustacluster-key root@$ip "echo -n" || true
-done
-
-eval $(ssh-agent)
-ssh-add ../repo-builder/imgs/bustacluster-key
-kubeVER=$kubeVER withMETRICS=$withMETRICS withPROMETHEUS=$withPROMETHEUS CLUSTERNAME=$CLUSTERNAME withCNI=$withCNI maxpods=$maxpods nodes="$(echo ${IPS[@]})" nodesIP="$(echo ${IPS[@]})" ../k8s-basic/kubeadm-cluster-install.sh
-ssh-agent -k
-
+# The kubeadm-cluster-install.sh script can, theoretically, be run from any host with passwordless ssh access to all nodes
+# However, we avoid running from the host OS in case it makes any undesirable changes to the host OS.
+#
+# The ansible playboos has already copied the kubeadm-cluster-install.sh script to the first node.
+ssh root@${IPS[0]} <<EOF_kubeadm
+kubeVER=$kubeVER withMETRICS=$withMETRICS withPROMETHEUS=$withPROMETHEUS CLUSTERNAME=$CLUSTERNAME withCNI=$withCNI maxpods=$maxpods nodes="$(echo ${IPS[@]})" nodesIP="$(echo ${IPS[@]})" /root/kubeadm-cluster-install.sh
+EOF_kubeadm
 
 #================================================================================================
 # collect kube.config and cluster certs.
-ssh -i ../repo-builder/imgs/bustacluster-key -o StrictHostKeyChecking=no root@${IPS[0]} "cat /etc/kubernetes/admin.conf" > kube.config
+ssh root@${IPS[0]} "cat /etc/kubernetes/admin.conf" > kube.config
 
-scp -i ../repo-builder/imgs/bustacluster-key -o StrictHostKeyChecking=no root@${IPS[0]}:/etc/kubernetes/pki/ca.{crt,key} .
+scp root@${IPS[0]}:/etc/kubernetes/pki/ca.{crt,key} .
 
 echo "Cluster kube-config copied to local directory" | tee instruct-${CLUSTERNAME}.txt
 echo "Cluster certs copied to local directory" | tee -a instruct-${CLUSTERNAME}.txt
 echo "To use the cluster:" | tee -a instruct-${CLUSTERNAME}.txt
 echo "  export KUBECONFIG=$(pwd)/kube.config" | tee -a instruct-${CLUSTERNAME}.txt
 
-
 #================================================================================================
 
 echo | tee -a instruct-${CLUSTERNAME}.txt
 echo "To connect:" | tee -a instruct-${CLUSTERNAME}.txt
 for ip in ${IPS[*]}; do
-  echo "ssh -i ../repo-builder/imgs/bustacluster-key -o StrictHostKeyChecking=no root@${ip}" | tee -a instruct-${CLUSTERNAME}.txt
+  echo "ssh root@${ip}" | tee -a instruct-${CLUSTERNAME}.txt
 done
 
 echo | tee -a instruct-${CLUSTERNAME}.txt
